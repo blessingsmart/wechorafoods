448f42c0d828da608300307f03085d94bf7dc7a2
Blessing Eno <bublessingsmart@gmail.com>
2024-02-06 17:04:03 +0100

    new file:   src/pages/signup.jsx 	modified:   src/routes.jsx

---
+++ src/pages/signup.jsx
@@ -0,0 +1,108 @@
+import { Link, useNavigate } from "react-router-dom";
+
+const SignUp = () => {
+    const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
+    const { mutateAsync: registerUser } = useRegisterMutation();
+
+    const { errors, register, handleSubmit } = useSubmit(SignUpSchema);
+
+    const onSubmit = async (data) => {
+        setLoading(true);
+        try {
+            const response = await registerUser(data);
+            if (!response) {
+                return;
+            }
+
+            SuccessToast(response?.message);
+            navigate("/auth-otp");
+        } catch (error) {
+            console.log(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <main className="main lg:pr-[5%]">
+            <section className="left-section">
+                <div className="imgg">
+                    <div className="vector"></div>
+                </div>
+            </section>
+            <form onSubmit={handleSubmit(onSubmit)} className="mx-auto w-[90%] max-w-[60rem] mt-8">
+                <div className="hdngs">
+                    <h2>WELCOME TO BOOKSLAB</h2>
+                    <p>Register your account</p>
+                </div>
+                <Input
+                    label="Name"
+                    name="name"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    autoComplete="name"
+                    placeholder="Name..."
+                />
+                <Input
+                    label="Username"
+                    name="username"
+                    type="text"
+                    register={register}
+                    errors={errors}
+                    placeholder="Username..."
+                    autoComplete="username"
+                />
+                <Input
+                    label="Email"
+                    name="email"
+                    type="email"
+                    register={register}
+                    errors={errors}
+                    autoComplete="email"
+                    placeholder="Email..."
+                />
+                {/* container for passwords */}
+                <div className="w-full flex flex-col md:flex-row justify-between gap-[5%] items-center">
+                    <div className="w-full">
+                        <Input
+                            label="Password"
+                            name="password"
+                            type="password"
+                            register={register}
+                            errors={errors}
+                            placeholder="Password..."
+                            id="new-password"
+                            autoComplete="new-password"
+                        />
+                    </div>
+                    <div className="w-full">
+                        <Input
+                            label=" Confirm Password"
+                            name="confirm_password"
+                            type="password"
+                            register={register}
+                            errors={errors}
+                            autoComplete="new-password"
+                            placeholder="Confirm Password..."
+                            id="new_password"
+                        />
+                    </div>
+                </div>
+                <Button borderVariant="noRadius" variant="primary" isLoading={loading}>
+                    Sign Up
+                </Button>{" "}
+                <h5 className="mb-8">
+                    Have an Account?
+                    <Link to="/login">
+                        {" "}
+                        <span>Login</span>
+                    </Link>
+                </h5>
+            </form>
+        </main>
+    );
+};
+
+export default SignUp;
--- src/routes.jsx
+++ src/routes.jsx
@@ -2,8 +2,9 @@ import React, { lazy } from "react";
 import Loading from "../src/components/loading";
 import { createBrowserRouter }  from "react-router-dom";
 
-const Home = lazy(() => import("./Pages/home"));
-const Login = lazy(() => import("./pages/login"));
+const Home = lazy(() => import("./pages/home"));
+const Signup = lazy(() => import("./pages/signup"));
+const Login = lazy(() => import("./pages/Login"));
 
 const BrowserRouter = createBrowserRouter([
     {
@@ -11,6 +12,14 @@ const BrowserRouter = createBrowserRouter([
         element: (
             <React.Suspense fallback={<Loading />}>
                 <Home />
+            </React.Suspense>
+        ),
+    },
+    {
+        path: "/signup",
+        element: (
+            <React.Suspense fallback={<Loading />}>
+                <Signup />
             </React.Suspense>
         ),
     },
