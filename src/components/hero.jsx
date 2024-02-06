8ba31f869d0b0ed632726f91a844e1ce2da6fb94
Blessing Eno <bublessingsmart@gmail.com>
2024-02-06 11:53:52 +0100

    new file:   src/assets/Kidneybeans.jpg 	modified:   src/components/hero.jsx

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Splide from '@splidejs/splide';
+import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+import '@splidejs/react-splide/css';
 import  food from "..//assets/food.png";
+import  beans from "..//assets/Kidneybeans.jpg";
 import { FaShoppingBasket } from "react-icons/fa";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
 const Hero = () => {
+    useEffect(() => {
+
+        const splide = new Splide('.splide', {
+          type: 'loop',
+          drag: 'free',
+          focus: 'center',
+          perPage: 1,
+          breakpoints: {
+            640: {
+              perPage: 1,},
+            },
+          autoScroll: {
+            speed: 1,
+          },
+        });
+    
+        splide.mount({ AutoScroll });
+      }, []); 
+
   return (
     <div className='flex justify-between m-20'>
         <div className='flex flex-col justify-center gap-10'>
             <div className='text-6xl font-semibold'>
-                <h1>Food Products, Services and Consulting</h1>
-                <span></span>
+                <h1>Food Products, </h1>
+                <h1>Services and Consulting</h1>
             </div>
                 <h2 className='text-gray-500'>We make home made and healthy food and drinks.</h2>
             <div className='flex gap-5'>
@@ -19,9 +42,27 @@ const Hero = () => {
                     Register <MdKeyboardArrowRight /> </button>
             </div>
         </div>
-        <img src={food} 
-            alt=""
-            className='rounded-2xl' />
+        <div name="Testimonials" className="splide w-1/2 bg-white rounded-lg">
+            <div className="splide__track">
+                <ul className="splide__list"> 
+                <li className="splide__slide px-1">
+                    {/* <div className=''>  */}
+                        <img src={food} alt="Image 1" className='rounded-lg w-96 '/>
+                    {/* </div> */}
+                </li>
+                <li className="splide__slide px-1">
+                    {/* <div className=''>  */}
+                        <img src={beans} alt="Image 2" className='rounded-lg w-96 '/>
+                    {/* </div> */}
+                </li>
+                <li className="splide__slide px-1">
+                    {/* <div className=''>  */}
+                        <img src={food} alt="Image 3" className='rounded-lg w-96 '/>
+                    {/* </div> */}
+                </li>
+                </ul>
+            </div>
+        </div>
     </div>
   )
 }
