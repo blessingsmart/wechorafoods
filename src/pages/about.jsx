63707920570ef9f6caad1e3ab603958d51767fce
Blessing Eno <bublessingsmart@gmail.com>
2024-02-12 10:49:06 +0100

    new file:   src/assets/mission.jpg 	new file:   src/assets/target.png 	modified:   src/pages/about.jsx

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,6 +3,9 @@ import Navbar from "../components/navbar"
 import Footer from "../components/footer"
 import  yummy  from "../assets/yummy.jpg";
 import  yummy1  from "../assets/yummy1.jpg";
+import  mission  from "../assets/mission.jpg";
+import { FaPlaneDeparture } from "react-icons/fa6";
+import { TbTargetArrow } from "react-icons/tb";
 
 const About = () => {
   return (
@@ -15,7 +18,22 @@ const About = () => {
             <p className='text-center font-medium'>Wechora Foods is a registered company and a member of the Nigeria Institute of Food Science and Technology.  We are a  food technology company that aims to revolutionize the way people consume and experience food by providing sustainable and healthy alternatives to traditional meals.</p>
             <button className='bg-orange-600 px-3 py-2 rounded-lg text-white'>See more</button>
         </div>
-        <img src={yummy} alt="" />
+        <img src={yummy} 
+        alt="" />
+    </div>
+    <div className='flex gap-5 justify-between mx-20 my-10'>
+        <div className='flex flex-col gap-12'>
+            <h2 className='font-bold text-5xl text-orange-600'>Our Target Audience</h2>
+            <p className='w-1/2'>Health-conscious individuals who prioritize sustainability and are looking for convenient and eco-friendly meal options.</p>
+        </div>
+        <TbTargetArrow className='w-52 h-52 text-orange-600' />
+    </div>
+    <div className='flex gap-5 justify-between mx-20 my-10'>
+        <FaPlaneDeparture className='w-52 h-52 text-orange-600' />
+        <div className='flex flex-col gap-12'>
+            <h2 className='font-bold text-5xl text-orange-600'>Our Mission</h2>
+            <p className='w-1/2'>Our environmentally-friendly alternatives helps busy lifestyles that hinders  healthy eating habits </p>
+        </div>
     </div>
 
     < Footer />
