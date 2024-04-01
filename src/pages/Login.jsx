45f024d2139c47a2b0dd20849aa56cd23960cffc
Blessing Eno <bublessingsmart@gmail.com>
2024-04-01 23:37:14 +0100

    modified:   src/pages/Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link as RouterLink} from "react-router-dom";
 import { useNavigate } from 'react-router-dom';
 import  food from "..//assets/logo.png";
 
